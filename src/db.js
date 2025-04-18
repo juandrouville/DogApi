ba772fb268f0d6b4f8924e320809fccd81324b22
juandrouville <jadrouville@gmail.com>
2025-04-18 13:29:51 -0300

    changes for service render

--- src/db.js
+++ src/db.js
@@ -1,12 +1,22 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 
-export const sequelize = 
- new Sequelize(
-    process.env.DB_URL,
-    {
-      loggin:false,
-      native:false,
-    }
-  ); 
+const sequelize = 
+ new Sequelize({
+  dialect:'postgres',
+  host:process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.nextTick.DB_PORT,
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },    
+  loggin:false,
+}); 
+  
+export default sequelize;
