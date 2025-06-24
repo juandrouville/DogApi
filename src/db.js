2dc2bfe813c707b4cc4bd12b6569235748d59559
juandrouville <jadrouville@gmail.com>
2025-06-24 20:00:54 -0300

    optimized archive db.js for vercel

--- src/db.js
+++ src/db.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 export const sequelize = 
  new Sequelize(process.env.DB_URL,{
+  dialect: 'postgres',
+  protocol:'postgres',
   dialectOptions:{
     ssl:{
       require: true,
