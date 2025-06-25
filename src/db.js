16ea9909538a33332d4d2665ab5679681ea96813
juandrouville <jadrouville@gmail.com>
2025-06-24 21:18:23 -0300

    update all

--- src/db.js
+++ src/db.js
@@ -5,7 +5,6 @@ import { Sequelize } from 'sequelize';
 export const sequelize = 
  new Sequelize(process.env.DB_URL,{
   dialect: 'postgres',
-  protocol:'postgres',
   dialectOptions:{
     ssl:{
       require: true,
