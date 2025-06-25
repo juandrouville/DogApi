38d008d35f5eec8bf34b39d9c63d30e038964626
juandrouville <jadrouville@gmail.com>
2025-06-25 19:40:21 -0300

    import pg and dialectmodules

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 export const sequelize = 
  new Sequelize(process.env.DB_URL,{
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions:{
     ssl:{
       require: true,
